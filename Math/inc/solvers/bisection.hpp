f4516704966f8f59ccf76c8d686c67033d6f5f05
Murillo Vinicius Bento Santana <mvbentosantana@gmail.com>
2025-01-15 18:20:06 +0100

    adde const args to bisection

--- Math/inc/solvers/bisection.hpp
+++ Math/inc/solvers/bisection.hpp
@@ -17,10 +17,13 @@ namespace math
 	public:
 		//solve
 		bool solve(void**);
+		bool solve(const void**);
 
 		//data
-		double(*m_system)(double, void**);
 		double m_x1, m_x2, m_xs, m_tolerance;
 		uint32_t m_iteration, m_iteration_max;
+
+		double(*m_system_1)(double, void**);
+		double(*m_system_2)(double, const void**);
 	};
 }
--- Math/src/solvers/bisection.cpp
+++ Math/src/solvers/bisection.cpp
@@ -8,7 +8,7 @@
 namespace math
 {
 	//constructors
-	bisection::bisection(void) : m_tolerance(1e-5), m_iteration_max(100)
+	bisection::bisection(void) : m_tolerance(1e-5), m_iteration_max(100), m_system_1(nullptr), m_system_2(nullptr)
 	{
 		return;
 	}
@@ -24,8 +24,8 @@ namespace math
 	{
 		//data
 		double fs;
-		double f1 = m_system(m_x1, args);
-		double f2 = m_system(m_x2, args);
+		double f1 = m_system_1(m_x1, args);
+		double f2 = m_system_1(m_x2, args);
 		//check
 		if(f1 * f2 > 0)
 		{
@@ -40,7 +40,32 @@ namespace math
 			if(fabs(f2) < m_tolerance) { m_xs = m_x2; return true; }
 			//update
 			m_xs = (m_x1 + m_x2) / 2;
-			fs = m_system(m_xs, args);
+			fs = m_system_1(m_xs, args);
+			if(fs * f1 > 0) { m_x1 = m_xs; f1 = fs; } else { m_x2 = m_xs; f2 = fs; }
+		}
+		return false;
+	}
+	bool bisection::solve(const void** args)
+	{
+		//data
+		double fs;
+		double f1 = m_system_2(m_x1, args);
+		double f2 = m_system_2(m_x2, args);
+		//check
+		if(f1 * f2 > 0)
+		{
+			printf("Initial guess creates outputs with the same sign\n");
+			return false;
+		}
+		//loop
+		for(m_iteration = 0; m_iteration < m_iteration_max; m_iteration++)
+		{
+			//test
+			if(fabs(f1) < m_tolerance) { m_xs = m_x1; return true; }
+			if(fabs(f2) < m_tolerance) { m_xs = m_x2; return true; }
+			//update
+			m_xs = (m_x1 + m_x2) / 2;
+			fs = m_system_2(m_xs, args);
 			if(fs * f1 > 0) { m_x1 = m_xs; f1 = fs; } else { m_x2 = m_xs; f2 = fs; }
 		}
 		return false;
