a4c40116f2d92ac401f831ab2ead034681339a09
Murillo Santana <mvbentosantana@gmail.com>
2026-07-09 09:13:16 +0200

    getting there

--- Test/src/solvers/GradientDescent/single_quadratic.cpp
+++ Test/src/solvers/GradientDescent/single_quadratic.cpp
@@ -4,28 +4,25 @@
 
 //Test
 #include "Math/Test/inc/solvers.hpp"
-#include "Math/inc/Validation/Validator.hpp"
 
 void tests::solvers::gradient_descent::single_quadratic(void)
 {
 	//data
-	math::validation::Validator validator;
 	math::solvers::GradientDescent solver;
-
+	
 	//solver
 	solver.m_size = 1;
 	solver.step_size(1.00e-01);
 	solver.iteration_max(1000);
 	solver.gradient([] (double* g, const double* x) { g[0] = x[0]; });
-
+	
 	//setup
 	solver.allocate();
 	solver.m_x_old[0] = 1.00e+00;
-
+	
 	//solve
 	solver.solve();
-	printf("Iteration: %d\n", solver.iteration());
-
+	
 	//print
 	double g;
 	solver.gradient(&g, solver.m_x_new);
